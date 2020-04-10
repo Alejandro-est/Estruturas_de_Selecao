e79d7956143e42c18024a41c6eb248ca112d8f1b
Alejandro R. E. Cristiano <alejandro.estevez.cristiano@gmail.com>
2020-04-10 18:04:45 -0300

    ok
    ok

---
+++ if_else_encadeado.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main ()
+{
+    int chute, sorteado;
+    printf("********************************************\n");
+    printf("**** Bem-vindo ao Jogo Da Adivinhacao! *****\n");
+    printf("********************************************\n");
+
+    printf("Digite um numero: \n");
+    scanf("%d",&chute);
+    sorteado=42;
+
+    if(chute==sorteado){
+        printf("Voce Acertou!!!\n");
+}
+     else{
+          if(chute<sorteado){
+            printf("Voce Perdeu!!! seu chute %d, e menor que o sorteado!!!\n",chute);
+            }
+            else{
+                printf("Voce Perdeu!!! seu chute %d, e maior que o sorteado!!!\n",chute);
+            }
+        }
+
+      Printf("*** GAME OVER ***/n");
+      return 0;
+    }
